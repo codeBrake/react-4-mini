20b766773bd1fde41d694fbbf01cb8e3a9a406b6
Austin <brake_austin@yahoo.com>
2018-10-30 15:31:50 -0600

    finished

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import routes from './routes';
+import {Link} from 'react-router-dom';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <Link to='/'>
+            <p>Route 1</p>
+          </Link>
+          <Link to='/2'>
+            <p>Route 2</p>
+          </Link>
+          <Link to='/3'>
+            <p>Route 3</p>
+          </Link>
+          {routes}
+          
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
